85d1d465064b402b8c4a9137a1dda7f1f90213fc
Matt Zukowski <matt@zukowski.ca>
2011-07-19 14:38:49 -0400

    refactored to use more generic fetchData and same origin detection; added fetchAllUsers method

--- sail.rollcall.js
+++ sail.rollcall.js
@@ -28,45 +28,66 @@ Sail.Rollcall.Client.prototype = {
     redirectToLogin: function() {        
         window.location.replace(this.url+'/login?destination='+escape(window.location.href))
     },
+    
+    
+    /**
+     * Determine whether we can talk to rollcall over REST
+     * or whetehr we have to use JSONP.
+     * Due to the same-origin policy in web browsers, REST can
+     * only be used if Rollcall is being served on the same
+     * domain as this script; otherwise JSONP must be used.
+     */
+    canUseREST: function() {
+        
+        // using jquery.url.js here
+        currentUrl = $.url.attr('source')
+        $.url.setUrl(this.url)
+        rollcallHost = $.url.attr('host')
+        rollcallPort = $.url.attr('port')
+        rollcallProtocol = $.url.attr('protocol')
+        $.url.setUrl(currentUrl)
+        
+        return rollcallHost == $.url.attr('host') 
+                && rollcallPort == $.url.attr('port')
+                && rollcallProtocol == $.url.attr('protocol')
+    },
 
 
     /**
      * Fetch session data for the given token.
      * If the session data is retrieved successfully, then given
      * callback is executed with the session data.
-     *
-     * The appropriate method for doign the request (JSON vs JSONP)
-     * will be automatically determined by comparing the Rollcall
-     * service URL to the current page's URL.
      */
     fetchSessionForToken: function(token, callback) {
-        currentUrl = $.url.attr('source')
-    
-        $.url.setUrl(this.url)
-        rollcallHost = $.url.attr('host')
-        rollcallPort = $.url.attr('port')
-        rollcallProtocol = $.url.attr('protocol')
-        $.url.setUrl(currentUrl)
-    
-        // determine whether we can talk to rollcall over REST
-        // or whetehr we have to use JSONP
-        if (rollcallHost == $.url.attr('host') 
-                && rollcallPort == $.url.attr('port')
-                && rollcallProtocol == $.url.attr('protocol')) {
-            this.fetchSessionForTokenUsingREST(token, callback)
+        url = this.url + '/sessions/validate_token.json'
+        
+        if (this.canUseREST()) {
+            this.fetchDataUsingREST(url, {token: token}, callback)
         } else {
-            this.fetchSessionForTokenUsingJSONP(token, callback)
+            this.fetchDataUsingJSONP(url, {token: token}, callback)
         }
     },
     
-    fetchSessionForTokenUsingREST: function(token, callback) {
-        var rollcall = this
-        return $.ajax({
-            url: rollcall.url + '/sessions/validate_token.json',
+    /**
+     * Fetch the list of all users registered on Rollcall.
+     */
+    fetchAllUsers: function(callback) {
+        url = this.url + '/users.json'
+        
+        if (this.canUseREST()) {
+            this.fetchDataUsingREST(url, {}, callback)
+        } else {
+            this.fetchDataUsingJSONP(url, {}, callback)
+        }
+    },
+     
+    fetchDataUsingREST: function(url, params, callback) {
+        rollcall = this
+        
+        $.ajax({
+            url: url,
             dataType: 'json',
-            data: {
-                token: token
-            },
+            data: params,
             success: callback,
             failure: function(error) {
                 console.log(error)
@@ -75,15 +96,15 @@ Sail.Rollcall.Client.prototype = {
         })
     },
     
-    fetchSessionForTokenUsingJSONP: function(token, callback) {
-        var rollcall = this
+    fetchDataUsingJSONP: function(url, params, callback) {
+        rollcall = this
+        
+        params['_method'] = params['_method'] || 'GET'
+        
         return $.ajax({
-            url: rollcall.url + '/sessions/validate_token.json',
+            url: url,
             dataType: 'jsonp',
-            data: {
-                _method: 'GET',
-                token: token
-            },
+            data: params,
             success: function(data) {
                 if (data.error) {
                     console.log(data)
