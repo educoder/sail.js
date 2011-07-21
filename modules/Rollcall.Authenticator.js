74dfd539b8b699b6a7d1d489facb84dd632794b0
Matt Zukowski <matt@zukowski.ca>
2011-07-21 13:18:54 -0400

    renamed Rollcall.LoginPicker to Rollcall.Authenticator and added error handler for when a user is already logged in

--- modules/Rollcall.Authenticator.js
+++ modules/Rollcall.Authenticator.js
@@ -1,11 +1,36 @@
-Rollcall.LoginPicker = {
+Rollcall.Authenticator = {
+    options: {mode: 'picker'},
+    
     events: {
         initialized: function(ev) {
-            Sail.loadCSS(Sail.modules.defaultPath + 'Rollcall.LoginPicker.css')
+            Sail.loadCSS(Sail.modules.defaultPath + 'Rollcall.Authenticator.css')
+        },
+        
+        connected: function(ev) {
+            Sail.Strophe.addErrorHandler(function(error, text) {
+                alert("The account '"+Sail.app.session.account.login+"' is already logged in.")
+                
+                Rollcall.Authenticator.requestLogin()
+            }, 'cancel', 'conflict')
         },
     },
     
-    showUserSelector: function(inContainer) {
+    unauthenticate: function() {
+        Sail.app.rollcall.destroySessionForToken(Sail.app.rollcall.getCurrentToken(), function() {
+            Sail.app.rollcall.unsetToken()
+            $(Sail.app).trigger('unauthenticated')
+        })
+    },
+    
+    requestLogin: function() {
+        // TODO: implement other modes (e.g. login)
+        if (Rollcall.Authenticator.options.mode == 'picker')
+            Rollcall.Authenticator.showUserPicker()
+        else
+            Rollcall.Authenticator.showUserPicker()
+    },
+    
+    showUserPicker: function(inContainer) {
         picker = $("<div id='login-picker' class='widget-box'></div>")
         picker.append("<h1 id='login-picker-instructions'>Log in as:</h1>")
         picker.append("<ul class='users'></ul>")
