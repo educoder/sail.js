75ffb408f21b91894469c20811391e2220309f50
Matt Zukowski <matt@zukowski.ca>
2011-08-30 11:29:22 -0400

    "login picker/user picker" --> "account picker"

--- modules/Rollcall.Authenticator.js
+++ modules/Rollcall.Authenticator.js
@@ -29,14 +29,14 @@ Rollcall.Authenticator = {
     requestLogin: function() {
         // TODO: implement other modes (e.g. login)
         if (Rollcall.Authenticator.options.mode == 'picker')
-            Rollcall.Authenticator.showUserPicker()
+            Rollcall.Authenticator.showAccountPicker()
         else
-            Rollcall.Authenticator.showUserPicker()
+            Rollcall.Authenticator.showAccountPicker()
     },
     
-    showUserPicker: function(inContainer) {
-        picker = $("<div id='login-picker' class='widget-box'></div>")
-        picker.append("<h1 id='login-picker-instructions'>Log in as:</h1>")
+    showAccountPicker: function(inContainer) {
+        picker = $("<div id='account-picker' class='widget-box'></div>")
+        picker.append("<h1 id='account-picker-instructions'>Log in as:</h1>")
         picker.append("<ul class='users'></ul>")
         
         Sail.app.rollcall.fetchAllUsers(function(data) {
@@ -44,25 +44,26 @@ Rollcall.Authenticator = {
                 u = this['user']
                 li = $("<li id='user-"+u.account.login+"'>"+u.account.login+"</li> ")
                 li.data('account', u.account)
-                li.click(function() {
-                    account = $(this).data('account')
-                    login = account.login
-                    password = account.password
-                    Sail.app.rollcall.createSession(login, password, function(session) {
-                        Sail.app.rollcall.setToken(session.token)
-                        Sail.UI.dismissDialog(picker)
-                        
-                        Sail.app.session = session
-                        
-                        $(Sail.app).trigger('authenticated')
-                    })
-                })
+                li.click(function() { Rollcall.Authenticator.pickLogin($(this).data('account')) })
                 picker.children(".users").append(li)
             })
             
             $(inContainer || 'body').append(picker)
             
             Sail.UI.showDialog(picker)
+        })
+    },
+    
+    pickLogin: function(account) {
+        login = account.login
+        password = account.password
+        Sail.app.rollcall.createSession(login, password, function(session) {
+            Sail.app.rollcall.setToken(session.token)
+            Sail.UI.dismissDialog(picker)
+            
+            Sail.app.session = session
+            
+            $(Sail.app).trigger('authenticated')
         })
     }
 }
