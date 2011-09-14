7af70f1c1fa01cbf687a159687c0b9c0d1d3e0de
Matt Zukowski <matt@zukowski.ca>
2011-09-14 09:51:24 -0400

    working multi-user authentication

--- modules/Rollcall.Authenticator.js
+++ modules/Rollcall.Authenticator.js
@@ -28,13 +28,23 @@ Rollcall.Authenticator = {
     
     requestLogin: function() {
         // TODO: implement other modes (e.g. login)
-        if (Rollcall.Authenticator.options.mode == 'picker')
-            Rollcall.Authenticator.showAccountPicker()
-        else
-            Rollcall.Authenticator.showAccountPicker()
+        switch (Rollcall.Authenticator.options.mode) {
+            case 'picker':
+                Rollcall.Authenticator.showAccountPicker()
+                break
+            case 'multi-picker':
+                Rollcall.Authenticator.showAccountPicker({multiPick: true})
+                break
+            default:
+                Rollcall.Authenticator.showAccountPicker()
+        }
     },
     
-    showAccountPicker: function(inContainer) {
+    showAccountPicker: function(options) {
+        options = options || {}
+        inContainer = options.inContainer || 'body'
+        multiPick = options.multiPick || false
+        
         picker = $("<div id='account-picker' class='widget-box'></div>")
         picker.append("<h1 id='account-picker-instructions'>Log in as:</h1>")
         picker.append("<ul class='users'></ul>")
@@ -44,26 +54,77 @@ Rollcall.Authenticator = {
                 u = this['user']
                 li = $("<li id='user-"+u.account.login+"'>"+u.account.login+"</li> ")
                 li.data('account', u.account)
-                li.click(function() { Rollcall.Authenticator.pickLogin($(this).data('account')) })
+                li.click(Rollcall.Authenticator.pickLogin)
                 picker.children(".users").append(li)
             })
             
-            $(inContainer || 'body').append(picker)
+            $(inContainer).append(picker)
             
             Sail.UI.showDialog(picker)
         })
+        
+        if (multiPick) {
+            picker.addClass('multi-picker')
+            
+            loginButtonContainer = $("<div><div>")
+            loginButtonContainer.css({
+                'text-align': 'center',
+                'background-color': '#666666',
+                'border-radius': '5px 5px 5px 5px',
+                'margin-top': '12px',
+                'padding': '5px'
+            })
+            loginButton = $("<button>Login</button>")
+            loginButton.css({
+                'font-weight': 'bold',
+            })
+            loginButton.button()
+            loginButton.click(Rollcall.Authenticator.commitLogin)
+            loginButtonContainer.append(loginButton)
+            picker.append(loginButtonContainer)
+        }
     },
     
-    pickLogin: function(account) {
-        login = account.login
-        password = account.password
-        Sail.app.rollcall.createSession(login, password, function(session) {
-            Sail.app.rollcall.setToken(session.token)
-            Sail.UI.dismissDialog(picker)
+    pickLogin: function() {
+        if (Rollcall.Authenticator.options.mode == 'multi-picker') {
+            $(this).toggleClass('selected')
+        } else {
+            account = $(this).data('account')
+            login = account.login
+            password = account.password
             
-            Sail.app.session = session
             
-            $(Sail.app).trigger('authenticated')
-        })
+            Sail.app.rollcall.createSession(account, Rollcall.Authenticator.loginFromSession)
+        }
+    },
+    
+    // only currently used when in multi-picker mode
+    commitLogin: function() {
+        accounts = $('#account-picker li.selected').map(function () {
+            return $(this).data('account').login
+        }).toArray()
+        
+        if (accounts.length == 0) {
+            alert("You must pick at least one account to log in!")
+            return
+        }
+        
+        ok = confirm("Log in as '"+accounts.join("' and '")+"'?")
+        if (ok) {
+            if (accounts.length > 1) {
+                Sail.app.rollcall.createGroupSession(accounts, Rollcall.Authenticator.loginFromSession)
+            } else {
+                Sail.app.rollcall.createSession(account, Rollcall.Authenticator.loginFromSession)
+            }
+        }
+    },
+    
+    loginFromSession: function(session) {
+        Sail.app.rollcall.setToken(session.token)
+        Sail.UI.dismissDialog(picker)
+    
+        Sail.app.session = session
+    
+        $(Sail.app).trigger('authenticated')
     }
 }
