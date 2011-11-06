d6f834450980660a24ba67c480426d51f3202101
Matt Zukowski <matt@zukowski.ca>
2011-11-06 10:58:37 -0500

    "UNHANDLED EVENT" messages are now a bit less alarming

--- sail.js
+++ sail.js
@@ -432,7 +432,7 @@ Sail.generateSailEventHandler = function(sailApp) {
             if (events && events[eventName]) // hacky way to check if a handler was bound for this event
                 $(sailApp).trigger(eventName, sev)
             else
-                console.log("UNHANDLED EVENT "+eventName, sev)
+                console.debug(sailApp.name+" is ignoring event "+eventName, sev)
                 
         } else if (typeof(mapping) == 'function') {
             mapping(sev)
