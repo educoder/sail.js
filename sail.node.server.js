751ebb48e35a86bc87d59b1ea36cd951ee1bafa2
Matt Zukowski <matt@zukowski.ca>
2011-11-06 01:33:29 -0500

    sail.node.server.js now passes JSLint

--- sail.node.server.js
+++ sail.node.server.js
@@ -11,102 +11,105 @@
     Looks for a `config.json` file in the current directory and configures itself accordingly.
 */
 
-var http = require('http')
-var httpProxy = require('http-proxy')
-var httpStatic = require('node-static')
-var url = require('url')
-var util = require('util')
-var fs = require('fs')
+/*global require, exports */
+
+var http = require('http');
+var httpProxy = require('http-proxy');
+var httpStatic = require('node-static');
+var url = require('url');
+var util = require('util');
+var fs = require('fs');
 
 try {
-    var config = JSON.parse(fs.readFileSync('config.json'))
+    var config = JSON.parse(fs.readFileSync('config.json'));
 } catch (e) {
-    console.error("Error reading config.json. Does the file exists?\n("+e+")")
-    throw e
+    console.error("Error reading config.json. Does the file exists?\n("+e+")");
+    throw e;
 }
 
-var proxy = new httpProxy.RoutingProxy()
-var file = new(httpStatic.Server)('.', {cache: false})
+var proxy = new httpProxy.RoutingProxy();
+var file = new(httpStatic.Server)('.', {cache: false});
 
 
 var server = http.createServer(function (req, res) {
-    for (i in server.proxyMap) {
-        map = server.proxyMap[i]
+    var i;
+    for (i = 0; i <= server.proxyMap.length; i++) {
+        var map = server.proxyMap[i];
         
         if (map.match(req)) {
-            map.proxy(req, res)
-            break
+            map.proxy(req, res);
+            break;
         }
     }
-})
+});
 
-server.config = config
-server.proxy = proxy
+server.config = config;
+server.proxy = proxy;
 
 server.start = function(port) {
     this.listen(port, function() {
-        console.log("\nUsing settings from config.js:\n", config, "\n")
-        console.log("Sail server listening on http://localhost:" + port + "...")
-    })
-}
+        console.log("\nUsing settings from config.js:\n", config, "\n");
+        console.log("Sail server listening on http://localhost:" + port + "...");
+    });
+};
 
 
 server.proxyMap = [
     {
         name: 'BOSH',
-        match: function(req) { return url.parse(req.url).pathname.match(/^\/http-bind/) },
+        match: function(req) { return url.parse(req.url).pathname.match(/^\/http-bind/); },
         proxy: function(req, res) {
-            console.log("PROXY "+req.url+" ==> "+config.bosh.url)
-            boshUrl = url.parse(config.bosh.url)
+            console.log("PROXY "+req.url+" ==> "+config.bosh.url);
+            var boshUrl = url.parse(config.bosh.url);
             proxy.proxyRequest(req, res, {
                 host: boshUrl.hostname,
                 port: boshUrl.port
-            })
+            });
         }
     },
     
     {
         name: "Rollcall",
-        match: function(req) { return url.parse(req.url).pathname.match(/^\/rollcall/) },
+        match: function(req) { return url.parse(req.url).pathname.match(/^\/rollcall/); },
         proxy: function(req, res) {
-            req.url = req.url.replace(/\/rollcall/,'')
-            rollcallUrl = url.parse(config.rollcall.url)
-            console.log("PROXY "+req.url+" ==> "+config.rollcall.url)
-            req.headers['host'] = rollcallUrl.hostname
+            req.url = req.url.replace(/\/rollcall/,'');
+            var rollcallUrl = url.parse(config.rollcall.url);
+            console.log("PROXY "+req.url+" ==> "+config.rollcall.url);
+            req.headers['host'] = rollcallUrl.hostname;
             proxy.proxyRequest(req, res, {
                 host: rollcallUrl.hostname,
                 port: rollcallUrl.port || 80
-            })
+            });
         }
     },
     
     {
         name: "Mongoose",
-        match: function(req) { return url.parse(req.url).pathname.match(/^\/mongoose/) },
+        match: function(req) { return url.parse(req.url).pathname.match(/^\/mongoose/); },
         proxy: function(req, res) {
-            req.url = req.url.replace(/\/mongoose/,'')
-            mongooseUrl = url.parse(config.mongoose.url)
-            console.log("PROXY "+req.url+" ==> "+config.mongoose.url)
-            req.headers['host'] = mongooseUrl.hostname
+            req.url = req.url.replace(/\/mongoose/,'');
+            var mongooseUrl = url.parse(config.mongoose.url);
+            console.log("PROXY "+req.url+" ==> "+config.mongoose.url);
+            req.headers['host'] = mongooseUrl.hostname;
             proxy.proxyRequest(req, res, {
                 host: mongooseUrl.hostname,
                 port: mongooseUrl.port
-            })
+            });
         }
     },
     
     {
         name: "STATIC",
-        match: function(req) { return true },
+        match: function(req) { return true; },
         proxy: function(req, res) {
             req.addListener('end', function(){ 
-                console.log("STATIC "+req.url)
-                file.serve(req, res)      
-            })
+                console.log("STATIC "+req.url);
+                file.serve(req, res);     
+            });
         }
     }
-]
+];
 
 
-exports.server = server
+exports.server = server;
 
