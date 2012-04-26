f051c1be170c57f46fd65405781512c5c2b998dc
Matt Zukowski <matt@zukowski.ca>
2012-04-26 18:44:44 -0400

    Pass jshint

--- sail.ui.js
+++ sail.ui.js
@@ -1,38 +1,40 @@
+/*jshint browser: true, devel: true */
+/*globals Sail, jQuery */
 Sail.UI = {
     init: function() {
-        $(document).ready(function() {
+        jQuery(document).ready(function() {
             // We need to turn off animation effect for Xoom tablets since they can't really handle it
             // FIXME: ugly way to check if we're on a Xoom tablet
             if (navigator.userAgent.match('Android'))
-                jQuery.fx.off = true
+                jQuery.fx.off = true;
             
-            $('button, input[type=submit], input[type=reset], input[type=button]').button()
-            $('.dialog button').click(function(){
-                Sail.UI.dismissDialog($(this).parents('.dialog'))
-            })
+            jQuery('button, input[type=submit], input[type=reset], input[type=button]').button();
+            jQuery('.dialog button').click(function () {
+                Sail.UI.dismissDialog(jQuery(this).parents('.dialog'));
+            });
             
-            $(Sail.app).trigger('initialized.ui')
-        })
+            jQuery(Sail.app).trigger('initialized.ui');
+        });
     },
     
     showDialog: function(jq) {
-        dialogId = 'sail-dialog-'+Math.floor(Math.random()*10e10).toString(16)
-        console.debug("Sail.UI: showing dialog "+dialogId)
-        mask = $('<div class="mask"></div>')
-        mask.addClass(dialogId)
-        mask.insertBefore(jq)
-        mask.css('z-index', 999)
-        $(jq).css('z-index', 1000)
-        $(jq).data('sail-dialog-id', dialogId)
-        $(jq).show()
+        var dialogId = 'sail-dialog-'+Math.floor(Math.random()*10e10).toString(16);
+        console.debug("Sail.UI: showing dialog "+dialogId);
+        var mask = jQuery('<div class="mask"></div>');
+        mask.addClass(dialogId);
+        mask.insertBefore(jq);
+        mask.css('z-index', 999);
+        jQuery(jq).css('z-index', 1000);
+        jQuery(jq).data('sail-dialog-id', dialogId);
+        jQuery(jq).show();
     },
     
     dismissDialog: function(jq) {
-        if ($(jq).length == 0)
-            return // don't attempt to dismiss if dialog doesn't exist
-        dialogId = $(jq).data('sail-dialog-id')
-        console.debug("Sail.UI: dismissing dialog "+dialogId)
-        $('.mask.'+dialogId).remove()
-        $(jq).fadeOut('fast', function() {$(this).remove()})
+        if (jQuery(jq).length === 0)
+            return; // don't attempt to dismiss if dialog doesn't exist
+        var dialogId = jQuery(jq).data('sail-dialog-id');
+        console.debug("Sail.UI: dismissing dialog "+dialogId);
+        jQuery('.mask.'+dialogId).remove();
+        jQuery(jq).fadeOut('fast', function() {jQuery(this).remove();});
     }
-}
+};
